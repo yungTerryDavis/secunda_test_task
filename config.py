920b929aa5002693f5a7383ae5c91b381bc86452
yungTerryDavis <chulkov.nikita01@gmail.com>
2026-01-20 16:00:49 +0300

    infra: add docker-compose, config, envs, deps

---
+++ config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config: SettingsConfigDict = SettingsConfigDict(env_file=".env")
+
+    def get_db_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+
+settings = Settings()
