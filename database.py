c91470e135175fa62bcb01f023aca0948d6d8706
yungTerryDavis <chulkov.nikita01@gmail.com>
2026-01-20 16:02:29 +0300

    db: add schema, migrations, db configuration, db methods

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from config import settings
+
+
+DATABASE_URL = settings.get_db_url()
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
