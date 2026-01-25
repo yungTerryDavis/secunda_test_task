edb7c44e0cc5202c033d25861baa74424c9428f2
yungTerryDavis <chulkov.nikita01@gmail.com>
2026-01-25 14:09:56 +0300

    feat(DB): add db session generator

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from config import settings
 
@@ -12,3 +12,8 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__: bool = True
+
+
+async def get_db_session():
+    async with async_session_maker() as session:
+        yield session
